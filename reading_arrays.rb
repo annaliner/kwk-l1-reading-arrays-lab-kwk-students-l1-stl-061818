12eb0f599874320e51b1dcf91f0059be3662eeb8
annaliner <annaliner89@gmail.com>
2018-06-20 15:40:03 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -17,18 +17,28 @@ def fourth_student_by_index
   puts STUDENT_NAMES[3]
 end
 
+fourth_student_by_index
+
 def last_student_by_index
   puts STUDENT_NAMES[5]
 end
+
+last_student_by_index
 
 def first_student_by_method
   puts STUDENT_NAMES.first
 end
 
+first_student_by_method
+
 def last_student_by_method
   puts STUDENT_NAMES.last
 end
 
+last_student_by_method
+
 def first_second_and_third_students
   puts STUDENT_NAMES[0,2]
 end
+
+first_second_and_third_students
