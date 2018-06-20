96b46e66871fc3149c77661f5bb8dce93be3c72c
annaliner <annaliner89@gmail.com>
2018-06-20 15:35:16 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -14,7 +14,7 @@ first_student_by_index
  
  
   # Write a solution that returns the first student in the array student_names
-end
+
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
